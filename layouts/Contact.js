629abbbceaceff005676ce783ee27a8ff636c7b8
Anneryshc <afh9@njit.edu>
2023-05-10 16:25:36 -0400

    paso en mailchimp

--- layouts/Contact.js
+++ layouts/Contact.js
@@ -1,39 +1,10 @@
 import config from "@config/config.json";
 import { markdownify } from "@lib/utils/textConverter";
-import { useState } from "react";
 
 const Contact = ({ data }) => {
   const { frontmatter } = data;
   const { title, info } = frontmatter;
-  const { contact_form_action, mailchimp_api_key, mailchimp_list_id } = config.params;
-  const [message, setMessage] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const data = {
-      email_address: formData.get("email"),
-      status: "subscribed",
-      merge_fields: {
-        FNAME: formData.get("name"),
-        SUBJECT: formData.get("subject")
-      }
-    };
-  const response = await fetch(`https://us21.api.mailchimp.com/3.0/lists/${mailchimp_list_id}/members`, {
-  method: "POST",
-  headers: {
-    Authorization: `apikey ${mailchimp_api_key}`,
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(data),
-});
-    const result = await response.json();
-    if (response.status === 200) {
-      setMessage("Thank you for subscribing!");
-    } else {
-      setMessage("Sorry, there was an error subscribing. Please try again.");
-    }
-  };
+  const { contact_form_action } = config.params;
 
   return (
     <section className="section">
@@ -45,7 +16,6 @@ const Contact = ({ data }) => {
               className="contact-form"
               method="POST"
               action={contact_form_action}
-              onSubmit={handleSubmit}
             >
               <div className="mb-3">
                 <input
@@ -85,7 +55,6 @@ const Contact = ({ data }) => {
                 Send Now
               </button>
             </form>
-            {message && <p>{message}</p>}
           </div>
           <div className="content col-12 md:col-6 lg:col-5">
             {markdownify(info.title, "h4")}
