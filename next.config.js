ebe376d0a068c2212e96629505e425fac795ea61
jar285 <jar285@njit.edu>
2023-05-11 02:18:54 -0400

    trying to fix i18n part1

--- next.config.js
+++ next.config.js
@@ -1,36 +1,17 @@
-/**
- * @type {import('next').NextConfig}
- */
-
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
-
 const withImages = require('next-images');
 
 module.exports = withImages({
+  reactStrictMode: true,
   basePath: process.env.NODE_ENV === 'production' ? '/webclass.org-template' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/webclass.org-template' : '',
   publicRuntimeConfig: {
     assetPrefix: process.env.NODE_ENV === 'production' ? '/webclass.org-template' : '',
   },
-});
-
-module.exports = {
-  // Your existing configurations...
-  basePath: "/webclass.org-template",
-  assetPrefix: "/webclass.org-template",
   images: {
     unoptimized: true,
-  },
-  publicRuntimeConfig: {
-    assetPrefix: "/webclass.org-template",
   },
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'en',
   },
-};
-
+});
