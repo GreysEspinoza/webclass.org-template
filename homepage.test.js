db7037bbafdb2770435d0f47edb2e12287499ec3
Jesus Adonis Rosario Vargas <114256420+jar285@users.noreply.github.com>
2023-05-11 05:55:56 -0400

    Delete homepage.test.js

--- homepage.test.js
+++
@@ -1,7 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('Homepage has correct title', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-  const title = await page.title();
-  expect(title).toBe('My Webclass');
-});
