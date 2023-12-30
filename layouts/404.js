bb30e2143491e2cdfdbb3f15d9a0759d813511c7
Jesus <jar285@njit.edu>
2023-12-30 12:23:46 -0500

    Temporarily fixed the about page, remember to change back the 404.js file to its originality

--- layouts/404.js
+++ layouts/404.js
@@ -1,20 +1,79 @@
-import { markdownify } from "@lib/utils/textConverter";
+import Link from "next/link";
+import Cta from "./components/Cta";
+import { markdownify } from '../lib/utils/helpers'; // Update the path to point to the lib/utils/helpers.js file
+import Image from "next/image";
+import getConfig from 'next/config';
+import { useTranslation } from "react-i18next";
+import pricingDataEn from "@config/pricing.json";
+import pricingDataEs from "@config/pricing.es.json";
 
-const NotFound = ({ data }) => {
-  const { frontmatter, content } = data;
+
+const { publicRuntimeConfig } = getConfig();
+const { assetPrefix } = publicRuntimeConfig;
+
+
+function Pricing() {
+
+  // get the current language from react-i18next
+  const { t, i18n } = useTranslation();
+
+  // set the pricing data based on the current language
+  const pricingData = i18n.language === "es" ? pricingDataEs : pricingDataEn;
+  const title = pricingData.title;
+  const feature = pricingData.feature;
+  const call_to_action = pricingData.call_to_action;
+
+  // Ensure feature is defined before rendering
+  if (!feature) {
+    return null;
+  }
 
   return (
-    <section className="section">
-      <div className="container">
-        <div className="flex h-[40vh] items-center justify-center">
-          <div className="text-center">
-            <h1 className="mb-4">{frontmatter.title}</h1>
-            {markdownify(content, "div", "content")}
+    <>
+      <section className="section pb-0">
+        <div className="container">
+          <h1 className="text-center font-normal">{title}</h1>
+          {/* Add the About description and other content here */}
+          <div className="about-content" style={{ lineHeight: "1.6", marginBottom: "2rem" }}>
+            {/* Other content */}
           </div>
-        </div>
-      </div>
-    </section>
-  );
-};
 
-export default NotFound;
+          {/* Features */}
+          <section className="section bg-theme-light">
+            <div className="container">
+              <div className="text-center">
+                <h2>{feature.title}</h2>
+              </div>
+              <div className="mt-8 grid gap-x-8 gap-y-6 sm:grid-cols-2 lg:grid-cols-3">
+                {feature.features.map((item, i) => (
+                  <div
+                    className="feature-card rounded-xl bg-white p-5 pb-8 text-center"
+                    key={`feature-${i}`}
+                  >
+                    {item.icon && (
+                      <Image
+                        className="mx-auto"
+                        src={`${assetPrefix}${item.icon}`}
+                        width={30}
+                        height={30}
+                        alt=""
+                      />
+                    )}
+                    <div className="mt-4">
+                      {markdownify(item.name, "h3", "h5")}
+                      <p className="mt-3">{item.content}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </section>
+        </div>
+      </section>
+      <Cta cta={call_to_action} />
+    </>
+  );
+}
+
+
+export default Pricing;
